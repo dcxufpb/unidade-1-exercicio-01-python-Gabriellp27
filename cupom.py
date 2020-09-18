556a5935e293ac9f5a313d27e919414327e49046
Gabriellp27 <lopesgabriel208@gmail.com>
2020-09-18 17:28:02 +0000

    Fiz as alterações necessárias no arquivo cupom.py

--- cupom.py
+++ cupom.py
@@ -1,3 +1,10 @@
 def imprime_dados_loja():
     # Implemente aqui
-    return "" 
+    print("Arcos Dourados Com. de Alimentos LTDA")
+    print("Av. Projetada Leste, 500 EUC F32/33/34")
+    print("Br. Sta Genebra - Campinas - SP")
+    print("CEP:13080-395 Tel (19) 3756-7408")
+    print("Loja 1317 (PDP)")
+    print("CNPJ: 42.591.651/0797-34")
+    print("IE: 244.898.500.113")
+    return
